4cd26a531ccde2a2abc6ab28060ee6a6624396b0
bayaman02 <nurikshurik131@gmail.com>
2024-01-15 15:57:44 +0600

    Add

--- app/src/main/java/com/classwork/mon3dz1/MainActivity.java
+++ app/src/main/java/com/classwork/mon3dz1/MainActivity.java
@@ -6,23 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 private Button button;
+private EditText et_odin;
+private EditText et_dva;
+private EditText et_three;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       button = findViewById(R.id.btn_galery);
+      et_odin = findViewById(R.id.et_odin);
+      et_dva = findViewById(R.id.et_2);
+      et_three = findViewById(R.id.et_three);
       button.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
               Intent intent = new Intent(Intent.ACTION_SEND);
-              intent.setType("plain/text");
-              intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "some@email.address" });
-              intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
-              intent.putExtra(Intent.EXTRA_TEXT, "mail body");
-              startActivity(Intent.createChooser(intent, "bro"));
+              intent.setType("email/*");
+              intent.putExtra(Intent.EXTRA_EMAIL, new String[] { et_odin.getText().toString()});
+              intent.putExtra(Intent.EXTRA_SUBJECT, et_dva.getText().toString());
+              intent.putExtra(Intent.EXTRA_TEXT, et_three.getText().toString());
+              startActivity((intent));
           }
       });
 
